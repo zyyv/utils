24ddc9eb15fca23c5b6b28f03d4e9fc3580919d7
隆杰 <953136447@qq.com>
2022-01-10 14:25:02 +0800

    feat:deepClone-perfect

--- test/is.test.ts
+++ test/is.test.ts
@@ -1,7 +1,10 @@
+import { isDate, isRegExp } from 'util/types'
 import { expect, it } from 'vitest'
 import { isPromise } from '../src'
 
 it('is', () => {
   const p = new Promise(() => {})
   expect(isPromise(p)).toBeTruthy()
+  expect(isRegExp(/123/g)).toBeTruthy()
+  expect(isDate(new Date())).toBe(true)
 })
