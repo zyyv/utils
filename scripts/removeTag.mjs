a9fdf449729f660dad3d70822ec1af8b185d80e1
chris-zhu <1633711653@qq.com>
2022-01-11 11:36:20 +0800

    fix: update gitignore

--- scripts/removeTag.mjs
+++
@@ -1,9 +0,0 @@
-import { $ } from 'zx'
-
-const result = await $`git tag`
-const tags = result.stdout.split('\n')
-
-tags.forEach(async(tag) => {
-  await $`git tag -d ${tag}`
-  await $`git push origin :${tag}`
-})
