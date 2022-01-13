7acf89844edc3713a1db10975bfe8d3f1037cef0
CHRISZHU\Chris <1633711653@qq.com>
2022-01-13 21:04:00 +0800

    feat: complete array

--- src/array/index.ts
+++ src/array/index.ts
@@ -11,3 +11,96 @@ export function toArray<T>(array?: Nullable<Arrayable<T>>): Array<T> {
     return array
   return [array]
 }
+
+/**
+ * Unique an Array
+ *
+ * @category Array
+ */
+export function uniq<T>(array: readonly T[]): T[] {
+  return Array.from(new Set(array))
+}
+
+/**
+ * Get nth item of Array. Negative for backward
+ *
+ * @category Array
+ */
+export function at(array: readonly [], index: number): undefined
+export function at<T>(array: readonly T[], index: number): T
+export function at<T>(array: readonly T[] | [], index: number): T | undefined {
+  const len = array.length
+  if (!len)
+    return undefined
+
+  if (index < 0)
+    index += len
+
+  return array[index]
+}
+
+/**
+ * Get last item
+ *
+ * @category Array
+ */
+export function last(array: readonly []): undefined
+export function last<T>(array: readonly T[]): T
+export function last<T>(array: readonly T[]): T | undefined {
+  return at(array, -1)
+}
+
+/**
+ * Remove an item from Array
+ *
+ * @category Array
+ */
+export function remove<T>(array: T[], value: T) {
+  if (!array)
+    return false
+  const index = array.indexOf(value)
+  if (index >= 0) {
+    array.splice(index, 1)
+    return true
+  }
+  return false
+}
+/**
+ * Genrate a range array of numbers. The `stop` is exclusive.
+ *
+ * @category Array
+ */
+export function range(stop: number): number[]
+export function range(start: number, stop: number, step?: number): number[]
+export function range(...args: any): number[] {
+  let start: number, stop: number, step: number
+
+  if (args.length === 1) {
+    start = 0
+    step = 1;
+    ([stop] = args)
+  } else {
+    ([start, stop, step = 1] = args)
+  }
+
+  const arr: number[] = []
+  let current = start
+  while (current < stop) {
+    arr.push(current)
+    current += step || 1
+  }
+
+  return arr
+}
+/**
+ * Move element in an Array
+ *
+ * @category Array
+ * @param arr
+ * @param from
+ * @param to
+ */
+export function move<T>(arr: T[], from: number, to: number) {
+  arr.splice(to, 0, arr.splice(from, 1)[0])
+  return arr
+}
--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,24 @@
 /**
- * Null or whatever
+ * Promise, or maybe not
  */
+export type Awaitable<T> = T | PromiseLike<T>
+
+/**
+  * Null or whatever
+  */
 export type Nullable<T> = T | null | undefined
 
 /**
- * Array, or not yet
- */
+  * Array, or not yet
+  */
 export type Arrayable<T> = T | Array<T>
-// /**
-//  * Array, or not yet
-//  */
-// export type Objectable<T> = T | Object<T>
+
+/**
+  * Function
+  */
+export type Fn<T = void> = () => T
+
+/**
+  * Constructor
+  */
+export type Constructor<T = void> = new (...args: any[]) => T
