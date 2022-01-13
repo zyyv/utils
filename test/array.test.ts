bb0e699ffb53a865a9d98d58298dce664fb28d0f
CHRISZHU\Chris <1633711653@qq.com>
2022-01-13 21:36:47 +0800

    chore: test update

--- test/array.test.ts
+++ test/array.test.ts
@@ -1,6 +1,13 @@
 import { expect, it } from 'vitest'
-import { toArray } from '../src'
+import { range, toArray } from '../src'
 
-it('Array', () => {
+it('toArray', () => {
   expect(toArray([0])).toEqual([0])
+})
+
+it('range', () => {
+  expect(range(0)).toEqual([])
+  expect(range(2)).toEqual([0, 1])
+  expect(range(2, 5)).toEqual([2, 3, 4])
+  expect(range(2, 10, 2)).toEqual([2, 4, 6, 8])
 })
