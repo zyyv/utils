2e56a8d69c7cc28e8d09c328c919d49979398a8d
zhao2513 <1124449695@qq.com>
2022-01-24 18:07:41 +0800

    feat(Typeof): add Typeof function

--- src/base/index.ts
+++ src/base/index.ts
@@ -8,3 +8,17 @@
  * @category Base
  */
 export const toTypeString = (v: any) => Object.prototype.toString.call(v)
+
+/**
+ * null => 'null'
+ * undefined => 'undefined'
+ * NaN => 'number'
+ * Number => 'number'
+ * String => 'string'
+ * Object => 'object'
+ * Function => 'function'
+ * Array => 'array'
+ * @param v any type
+ * @returns string
+ */
+export const Twypeof = (v: any): string => Object.prototype.toString.call(v).slice(8, -1).toLocaleLowerCase()
