62abf8f7bbeec894a74a3a4e97a823c539c6f66d
chris-zhu <1633711653@qq.com>
2022-06-29 13:45:19 +0800

    chore: update

--- src/date/index.ts
+++ src/date/index.ts
@@ -9,7 +9,7 @@ export const isLeapYear = (y: number) => (y % 4 === 0 && y % 100 !== 0) || y % 4
 /**
  * 简单的延迟函数
  * @param ms 毫秒数
- * @returns Promise<Unknow>
+ * @returns Promise<void>
  * @category Date
  */
-export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms))
+export const sleep = (ms: number) => new Promise<void>(resolve => setTimeout(resolve, ms))
--- test/string.test.ts
+++ test/string.test.ts
@@ -1,4 +1,4 @@
-import { assert, describe, expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { randomStr } from '@/string'
 
 describe('string', () => {
