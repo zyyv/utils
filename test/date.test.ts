5e9e1081e241d8edeb7957c7ebe9dd5532bf57c7
Chris <1633711653@qq.com>
2022-09-02 13:25:34 +0800

    feat(date): format timestamp and seconds

---
+++ test/date.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'vitest'
+import { formatSeconds, formatTimestamp } from '@/date'
+
+describe('date', () => {
+  test('formatTime', () => {
+    const timestamp = 1662096261943
+
+    expect(formatTimestamp(timestamp)).toMatchInlineSnapshot('"2022-09-02 13:24:21"')
+    expect(formatTimestamp(timestamp, 'yyyy-MM-dd')).toMatchInlineSnapshot('"2022-09-02"')
+  })
+
+  test('formatSeconds', () => {
+    expect(formatSeconds(60)).toMatchInlineSnapshot('"00:01:00"')
+    expect(formatSeconds(120)).toMatchInlineSnapshot('"00:02:00"')
+    expect(formatSeconds(58)).toMatchInlineSnapshot('"00:00:58"')
+    expect(formatSeconds(2120)).toMatchInlineSnapshot('"00:35:20"')
+    expect(formatSeconds(3600 * 24)).toMatchInlineSnapshot('"24:00:00"')
+  })
+})
